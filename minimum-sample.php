14fd8b020812182a3156dcade6dcdb84b872ddab
Akira Ouchi <akkiesoft@marokun.net>
2012-06-10 13:16:05 +0900

    Add minimum sample

---
+++ minimum-sample.php
@@ -0,0 +1,29 @@
+<?php
+session_start();
+if (isset($_POST['open'])) {
+	$ret = exec('/usr/sbin/eject -T /dev/sr0');
+	$ret = ($ret == "") ? "Success." : "Failure.";
+	$_SESSION['result'] = '<div id="result">Open ' . $result . '</div>';
+	header('Location:./');
+	exit();
+}
+?>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Web Eject</title>
+</head>
+<body>
+	<h1>Web Eject Button</h1>
+<?php
+if (isset($_SESSION['result'])) {
+	print $_SESSION['result'];
+	$_SESSION['result'] = '';
+}
+?>
+	<form action="./" method="post" id="aircon">
+		<input type="submit" name="open" value="Ejectする">
+	</form>
+</body>
+</html>
+
